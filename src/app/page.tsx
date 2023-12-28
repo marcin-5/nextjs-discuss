4fc5e606b99d065f300ed704ffd51e568c85e701
Marcin Bojara <marcin_5@mail.ru>
2023-12-28 18:10:11 +0100

    setup NextUI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from '@nextui-org/react';
+
 export default function HomePage() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <Button>Click me!!!</Button>
+    </div>
+  );
 }
