b4a6abde24df861804f1be54a1d3157532e4cc1c
Marcin Bojara <marcin_5@mail.ru>
2023-12-29 19:10:00 +0100

    add header component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import Providers from '@/app/providers';
+import Header from '@/components/header';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
@@ -14,7 +15,12 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>{children}</Providers>
+        <div className="container mx-auto px-4 max-w-6xl">
+          <Providers>
+            <Header />
+            {children}
+          </Providers>
+        </div>
       </body>
     </html>
   );
---
+++ src/components/header.tsx
@@ -0,0 +1,53 @@
+import { auth } from '@/auth';
+import {
+  Avatar,
+  Button,
+  Input,
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+} from '@nextui-org/react';
+import Link from 'next/link';
+
+export default async function Header() {
+  const session = await auth();
+
+  let authContent: React.ReactNode;
+  if (session?.user) {
+    authContent = <Avatar src={session.user.image || ''} />;
+  } else {
+    authContent = (
+      <>
+        <NavbarItem>
+          <Button type="submit" color="secondary" variant="bordered">
+            Sign In
+          </Button>
+        </NavbarItem>
+
+        <NavbarItem>
+          <Button type="submit" color="primary" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </>
+    );
+  }
+
+  return (
+    <Navbar className="shadow mb-6">
+      <NavbarBrand>
+        <Link href="/" className="font-bold">
+          Discuss
+        </Link>
+      </NavbarBrand>
+      <NavbarContent justify="center">
+        <NavbarItem>
+          <Input />
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarContent justify="end">{authContent}</NavbarContent>
+    </Navbar>
+  );
+}
