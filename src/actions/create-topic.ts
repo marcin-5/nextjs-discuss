5054257f45a62ee2d941f3de48f0f9705a18f890
Marcin Bojara <marcin_5@mail.ru>
2023-12-30 09:34:22 +0100

    handle database errors

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,6 +1,11 @@
 'use server';
 
 import { auth } from '@/auth';
+import { db } from '@/db';
+import paths from '@/paths';
+import type { Topic } from '@prisma/client';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 import { z } from 'zod';
 
 const createTopicSchema = z.object({
@@ -45,9 +50,30 @@ export async function createTopic(
     };
   }
 
-  return {
-    errors: {},
-  };
+  let topic: Topic;
+  try {
+    topic = await db.topic.create({
+      data: {
+        slug: result.data.name,
+        description: result.data.description,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        errors: {
+          _form: [err.message],
+        },
+      };
+    } else {
+      return {
+        errors: {
+          _form: ['Something went wrong'],
+        },
+      };
+    }
+  }
 
-  // TODO: revalidate the homepage
+  revalidatePath('/');
+  redirect(paths.topicShow(topic.slug));
 }
