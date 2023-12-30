6b632b210845aa5b319f311aaf22c992d92e347d
Marcin Bojara <marcin_5@mail.ru>
2023-12-30 16:18:03 +0100

    add post to database

--- src/actions/create-post.ts
+++ src/actions/create-post.ts
@@ -2,6 +2,10 @@
 
 import { auth } from '@/auth';
 import { db } from '@/db';
+import paths from '@/paths';
+import type { Post } from '@prisma/client';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 import { z } from 'zod';
 
 const createPostSchema = z.object({
@@ -54,9 +58,32 @@ export async function createPost(
     };
   }
 
-  return {
-    errors: {},
-  };
+  let post: Post;
+  try {
+    post = await db.post.create({
+      data: {
+        title: result.data.title,
+        content: result.data.content,
+        userId: session.user.id,
+        topicId: topic.id,
+      },
+    });
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      return {
+        errors: {
+          _form: [err.message],
+        },
+      };
+    } else {
+      return {
+        errors: {
+          _form: ['Failed to create post'],
+        },
+      };
+    }
+  }
 
-  // TODO: revalidate the topic show page
+  revalidatePath(paths.topicShow(slug));
+  redirect(paths.postShow(slug, post.id));
 }
