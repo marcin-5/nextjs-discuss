8b9bc3ccd464c23ead6c72146683dbe967e519c0
Marcin Bojara <marcin_5@mail.ru>
2023-12-31 09:43:14 +0100

    display comments

--- src/app/topics/[slug]/posts/[postId]/page.tsx
+++ src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -1,5 +1,7 @@
 import CommentCreateForm from '@/components/comments/comment-create-form';
+import CommentList from '@/components/comments/comment-list';
 import PostShow from '@/components/posts/post-show';
+import { fetchCommentsByPostId } from '@/db/queries/comments';
 import paths from '@/paths';
 import Link from 'next/link';
 
@@ -20,6 +22,7 @@ export default async function PostShowPage({ params }: PostShowPageProps) {
       </Link>
       <PostShow postId={postId} />
       <CommentCreateForm postId={postId} startOpen />
+      <CommentList fetchData={() => fetchCommentsByPostId(postId)} />
     </div>
   );
 }
