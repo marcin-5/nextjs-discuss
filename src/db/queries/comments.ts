0cef86cfd45d4a18f133bbd10de889aa7b9c5c61
Marcin Bojara <marcin_5@mail.ru>
2023-12-31 12:55:48 +0100

    cache results from database

--- src/db/queries/comments.ts
+++ src/db/queries/comments.ts
@@ -1,20 +1,23 @@
 import { db } from '@/db';
 import { Comment } from '@prisma/client';
+import { cache } from 'react';
 
 export type CommentWithAuthor = Comment & {
   user: { name: string | null; image: string | null };
 };
 
-export function fetchCommentsByPostId(postId: string): Promise<CommentWithAuthor[]> {
-  return db.comment.findMany({
-    where: { postId },
-    include: {
-      user: {
-        select: {
-          name: true,
-          image: true,
+export const fetchCommentsByPostId = cache(
+  (postId: string): Promise<CommentWithAuthor[]> => {
+    return db.comment.findMany({
+      where: { postId },
+      include: {
+        user: {
+          select: {
+            name: true,
+            image: true,
+          },
         },
       },
-    },
-  });
-}
+    });
+  }
+);
